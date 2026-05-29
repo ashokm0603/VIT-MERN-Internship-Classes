d904eee3be88013e15bc9da80b4aed3a326c8330
Ashoka M <ashokashokm2001@gmail.com>
2026-05-29 20:09:45 +0530

    day 8 and day 9 class code

---
+++ Day-8/script.js
@@ -0,0 +1,25 @@
+let a=10;
+let str="HELLO"
+console.log(a + ": a variable Value",a);
+
+
+ a+=a;// a=a+a;
+console.log("This is External JS file");
+
+console.log(a);//20
+
+a+=50;// a=a+50
+console.log(a);
+
+
+let b=true;
+let c=1;
+console.log(b==c);//
+console.log(b===c);
+
+
+console.log(10|5);
+
+
+
+
